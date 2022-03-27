eb8dbf49546afb3e586ed8b9b38bb96e9a1e0c31
Karina Pereira <karina.pereira93@hotmail.com>
2022-03-27 19:50:28 -0300

    Atualizacao Karina

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFCBDB',
+    backgroundColor: '#4169E1',
     alignItems: 'center',
     justifyContent: 'center',
   },
